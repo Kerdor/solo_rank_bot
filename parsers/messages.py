f90972f7289fdc44980ce5f3bbda1aff7c20421e
Nikita <59965225+Kerdor@users.noreply.github.com>
2026-08-29 22:03:05 +0300

    Split message parsers

---
+++ parsers/messages.py
@@ -0,0 +1,24 @@
+"""Маркеры и классификация сообщений бота."""
+
+WARN_NO_ENERGY_AND_HP = "Недостаточно энергии и низкое HP"
+WARN_NO_ENERGY = "Недостаточно энергии"
+WARN_LOW_HP = "Низкое HP"
+
+ALREADY_IN_DUNGEON_MARKER = "уже находишься в данже"
+INJURY_MARKER = "Раны заживут через"
+HOT_SPRINGS_BUTTON = "Горячие источники"
+ALREADY_IN_SPRINGS_MARKER = "Горячие источники: сеанс"
+RECOVERY_DONE_MARKER = "Восстановление завершено"
+INJURY_STATUS_MARKER = "Травма:"
+START_RECOVERY_BUTTON = "Начать восстановление"
+ENTER_SUCCESS_MARKER = "Инстанс-данж запущен"
+REPORT_MARKER = "Отчёт инстанс-данжа"
+
+
+def classify_warning(text: str):
+    """Возвращает (need_energy, need_hp) или (False, False)."""
+    if WARN_NO_ENERGY_AND_HP in text:
+        return True, True
+    need_energy = WARN_NO_ENERGY in text
+    need_hp = WARN_LOW_HP in text
+    return need_energy, need_hp
